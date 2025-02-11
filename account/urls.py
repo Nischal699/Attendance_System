229af02c4efe37dd547289b80996636fe951c366
Nischal Moktan <nischal123321@gmail.com>
2025-02-11 21:52:32 +0545

    commited

---
+++ account/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.homePage,name='home'),
+    path('about/', views.about,name='about'),
+    path('services/', views.services,name='services'),
+    path('portfolio/', views.portfolio,name='portfolio'),
+    path('contact/', views.contact,name='contact'),
+    
+    path('loginpage/', views.loginPage,name='loginPage'),
+    path('login/', views.auth_login,name='login'),
+    path('logout/', views.logout,name='logout'),
+    path('register/', views.register,name='register'),
+    path('adminpage/', views.adminPage, name='adminPage'),
+    path('student/', views.studentPage, name='studentPage'),
+    path('teacher/', views.teacherPage, name='teacherPage'),
+]
---
+++ account/views.py
@@ -0,0 +1,87 @@
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render,redirect
+from .forms import SignUpForm,LoginForm
+from django.core.paginator import Paginator
+from django.contrib.auth import authenticate,login,logout
+
+
+def homePage(request):
+    return render(request,"index.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def services(request):
+    return render(request,"services.html")
+
+def saveEnquiry(request):
+    return render(request,"contact.html")
+
+def portfolio(request):
+    data={
+        'title':'Contact',
+        'name':'Nischal'
+    }
+    return render(request,"portfolio.html",data)
+
+def contact(request):
+    data={
+        'title':'Contact',
+        'name':'Nischal'
+    }
+    return render(request,"contact.html",data)
+
+def register(request):
+    msg=None
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            msg = 'user created'
+            return redirect('login')
+        
+        else:
+            msg = 'form is not valid'
+    else:
+        form = SignUpForm()
+    return render(request,'register.html',{'form':form,'msg':msg})
+
+def auth_login(request):
+    form = LoginForm(request.POST or None)
+    msg =  None
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=password)
+            if user is not None and user.is_admin:
+                login(request, user)
+                return redirect('adminPage')
+            elif user is not None and user.is_student:
+                login(request, user)
+                return redirect('studentPage')
+            elif user is not None and user.is_teacher:
+                login(request, user)
+                return redirect('teacherPage')
+            else:
+                msg = 'invalid credentials'
+        else:
+            msg = 'error validation form'
+    return render(request,'login.html',{'form':form,'msg':msg})
+
+def logout(request):
+    return render(request,'index.html')
+
+def adminPage(request):
+    return render(request,'adminLogin.html')
+
+
+def teacherPage(request):
+    return render(request,'teacherLogin.html')
+
+
+def studentPage(request):
+    return render(request,'studentLogin.html')
+
+def loginPage(request):
+    return render(request,'loginPage.html')
